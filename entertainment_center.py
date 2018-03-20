e57dc464ce6a1695a4b1f9f693dd996eacf6ac58
ZwiFTi <bomarkandre@gmail.com>
2018-03-20 07:29:08 +0100

    bugfix

--- entertainment_center.py
+++ entertainment_center.py
@@ -9,29 +9,33 @@ s1 = media.Movie("Sharknado",
                  "wikipedia/en/9/93/Sharknado_poster.jpg",
                  "https://www.youtube.com/watch?v=M-pXDoe5a0E")
 
-s2 = media.Movie("Sharknado 2: The Second One",
+s2 = media.Movie("Sharknado 2",
                  "https://upload.wikimedia.org/"
                  "wikipedia/en/3/35/Sharknado_2_poster.jpg",
                  "https://www.youtube.com/watch?v=U3-7-kf6tVc")
 
-s3 = media.Movie("Sharknado 3: Oh Hell No!",
+s3 = media.Movie("Sharknado 3",
                  "https://upload.wikimedia.org/"
                  "wikipedia/en/5/5e/Sharkando_3_poster.jpg",
                  "https://www.youtube.com/watch?v=gP2j5lyKwKM")
 
-s4 = media.Movie("Sharknado 4: The 4th Awakens",
+s4 = media.Movie("Sharknado 4",
                  "https://upload.wikimedia.org/"
                  "wikipedia/en/4/4e/Sharkando_4_poster.jpg",
                  "https://www.youtube.com/watch?v=YwIsEPNHGsc")
 
-s5 = media.Movie("Sharknado 5: Global Swarming",
+s5 = media.Movie("Sharknado 5",
                  "https://images-na.ssl-images-amazon.com/images/"
                  "M/MV5BMjQ3Mzk5NzAwNV5BMl5BanBnXkFtZTgwNDkwOTc3MjI"
                  "@._V1_UX182_CR0,0,182,268_AL_.jpg",
                  "https://www.youtube.com/watch?v=hbRroXtmvOU")
 
+s6 = media.Movie("Inception",
+                 "https://upload.wikimedia.org/wikipedia/en/2/2e/"
+                 "Inception_%282010%29_theatrical_poster.jpg",
+                 "https://www.youtube.com/watch?v=YoHD9XEInc0")
 
-movies = [s1, s2, s3, s4, s5]
+movies = [s1, s2, s3, s4, s5, s6]
 
 if __name__ == "__main__":
     # Open website in browser, fill in with movies
